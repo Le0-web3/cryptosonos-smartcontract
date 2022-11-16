c886774727132d7942b7cb951a73b30e1abdab20
luckyclutcher <luckyclutcher@protonmail.com>
2022-11-16 18:21:27 -0500

    deploy to aurora testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -10,3 +10,22 @@ module.exports = {
     },
   },
 };
+
+module.exports = {
+  solidity: '0.8.0',
+  networks: {
+    aurora_testnet: {
+      url: 'https://testnet.aurora.dev',
+      accounts: [`0x${process.env.AURORA_TESTNET_PRIVATE_KEY}`],
+      chainId: 1313161555
+    },
+    aurora_mainnet: {
+      url: process.env.AURORA_RPC_URL,      
+      accounts: [`0x${process.env.AURORA_MAINNET_PRIVATE_KEY}`],
+      chainId: 1313161554,
+    },
+  },
+  etherscan: {
+    apiKey: process.env.AURORASCAN_API_KEY,
+  },
+};
